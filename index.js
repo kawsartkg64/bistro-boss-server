af1718cfa89e5f08f7be7729931a76b0d54eaa46
Md Kawsar Ali <kawsartkg@gmail.com>
2024-02-11 22:07:02 +0600

    add item and manage item api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -12,9 +13,9 @@ app.use(express.json())
 
 
 const { MongoClient, ServerApiVersion, Collection, ObjectId } = require('mongodb');
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hvjsse5.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hvjsse5.mongodb.net/?retryWrites=true&w=majority`
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -24,80 +25,180 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+
   try {
+
     const menuCollection = client.db('BistrobossDB').collection('menu')
     const reviewCollection = client.db('BistrobossDB').collection('review')
     const cartCollection = client.db('BistrobossDB').collection('Cart')
     const userCollection = client.db('BistrobossDB').collection('user')
 
+    // middleware verify token api
+    const verifyToken = (req, res, next) => {
+      if (!req.headers.authorization) {
 
-    app.get('/menu', async(req, res)=>{
-        const cursor = menuCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
-    })
-
-    app.get('/review', async(req, res)=>{
-        const result = await reviewCollection.find().toArray()
-        res.send(result)
-    })
-    // user admin api
-    app.patch('/users/admin/:id', async(req,res)=>{
-      const id = req.params.id;
-      const query ={_id: new ObjectId(id)};
-      const updateDoc ={
-        $set:{
-          role:"admin"
-        }
+        return res.status(401).send({ message: 'forbidden access' })
       }
-      const result = await userCollection.updateOne(query, updateDoc)
-      res.send(result)
-    })
+      const token = req.headers.authorization.split(' ')[1]
 
-    // user Collection
-    app.get('/users', async(req, res)=>{
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded
+        next();
+
+      })
+
+    }
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await userCollection.findOne(query)
+      const isAdmin = user?.role === 'admin'
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+      next()
+    }
+
+
+    // user related api
+    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+      console.log(req.headers)
       const result = await userCollection.find().toArray()
       res.send(result)
-        })
+    })
 
-    app.post('/users', async(req, res)=>{
-      const user = req.body;
-      const query = {email: user.email}
+    app.post('/users', async (req, res) => {
+      const user = req.body
+      const query = { email: user.email }
       const existingUser = await userCollection.findOne(query)
-      if(existingUser){
-        return res.send({message: 'User Already Exist', insertedId:null})
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
       }
-      const result= await userCollection.insertOne(user)
+      const result = await userCollection.insertOne(user)
       res.send(result)
     })
-    app.delete('/users/:id', async (req, res)=>{
-      const id = req.params.id
-      const query ={_id: new ObjectId(id)};
+    app.delete('/users/:id', verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query)
       res.send(result)
     })
 
+    // jwt related api
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token })
+    })
+
+    // admin related api
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== req.decoded.email) {
+        return res.status(403).send(({ message: 'unauthorized access' }))
+      }
+      const query = { email: email };
+      const user = await userCollection.findOne(query)
+      let admin = false
+      if (user) {
+        admin = user?.role === 'admin'
+      }
+      res.send({ admin })
+    })
+
+
+    app.patch('/users/admin/:id', async (req, res) => {
+
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          role: "admin"
+        }
+      }
+      const result = await userCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+
+
+
+
+    app.get('/menu', async (req, res) => {
+      const cursor = menuCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    
+    app.get('/menu/:id', async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) }
+      const result = await menuCollection.findOne(query)
+      console.log(result)
+      res.send(result)
+    })
+
+    app.post('/menu', async (req, res) => {
+      const item = req.body;
+      const result = await menuCollection.insertOne(item)
+      res.send(result)
+    })
+
+    app.patch('/menu/:id', async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          recipe: item.recipe,
+          image: item.image,
+
+        }
+      }
+      const result = await menuCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
+
+
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    app.get('/review', async (req, res) => {
+      const result = await reviewCollection.find().toArray()
+      res.send(result)
+    })
 
 
 
     // cart collection
 
-    app.get('/carts', async(req, res)=>{
+    app.get('/carts', async (req, res) => {
       const email = req.query.email;
-      const query = {email:email}
+      const query = { email: email }
       const result = await cartCollection.find(query).toArray()
       res.send(result)
     })
 
-    app.post('/carts', async(req, res)=>{
+    app.post('/carts', async (req, res) => {
       const cartItem = req.body
       const result = await cartCollection.insertOne(cartItem);
       res.send(result)
 
     })
-    app.delete('/carts/:id', async(req, res)=>{
+    app.delete('/carts/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query)
       res.send(result)
     })
@@ -107,7 +208,7 @@ async function run() {
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    
+
   }
 }
 run().catch(console.dir);
@@ -119,12 +220,12 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
-    res.send("Bistro Boss Server Running")
+app.get('/', (req, res) => {
+  res.send("Bistro Boss Server Running")
 
 })
 
 
-app.listen(port, ()=>{
-    console.log(`Bistro Boss Server Runnig on Post, ${port}`)
+app.listen(port, () => {
+  console.log(`Bistro Boss Server Runnig on Post, ${port}`)
 })
